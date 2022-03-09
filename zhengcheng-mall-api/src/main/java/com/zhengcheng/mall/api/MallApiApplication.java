7ff7f4923945ae3ddea2245b564ba63383a43460
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-03-09 13:45:50 +0800

    init 2.5.10

--- zhengcheng-mall-api/src/main/java/com/zhengcheng/mall/api/MallApiApplication.java
+++ zhengcheng-mall-api/src/main/java/com/zhengcheng/mall/api/MallApiApplication.java
@@ -1,9 +1,10 @@
 package com.zhengcheng.mall.api;
 
+import com.zhengcheng.core.annotation.EnableZhengChengSpringBootService;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
-import com.zhengcheng.core.annotation.EnableZhengChengService;
+import com.zhengcheng.core.annotation.EnableZhengChengSpringBootService;
 
 /**
  * MallApiApplication
@@ -11,7 +12,7 @@ import com.zhengcheng.core.annotation.EnableZhengChengService;
  * @author quansheng1.zhang
  * @since 2022/3/8 17:36
  */
-@EnableZhengChengService
+@EnableZhengChengSpringBootService
 @NacosPropertySource(dataId = "zhengcheng-mall", autoRefreshed = true)
 public class MallApiApplication {
 
