bc6177f39b9f1b3d62ddb42bd7ea7981c361ab5b
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-03-10 17:18:09 +0800

    转成springcloud服务

--- zhengcheng-mall-api-provider/src/main/java/com/zhengcheng/mall/api/feign/OauthFeignClientFallbackFactory.java
+++ zhengcheng-mall-api-provider/src/main/java/com/zhengcheng/mall/api/feign/OauthFeignClientFallbackFactory.java
@@ -1,11 +1,11 @@
 package com.zhengcheng.mall.api.feign;
 
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
 
 import com.zhengcheng.common.web.Result;
 import com.zhengcheng.mall.api.dto.TokenInfoDTO;
 
-import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 
 /**
