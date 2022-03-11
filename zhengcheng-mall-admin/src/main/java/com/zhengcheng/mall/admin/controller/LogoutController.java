51c52fccba8602736278611bea76db67d0d8a77b
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-03-11 19:29:22 +0800

    根据token获取登录信息

---
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/controller/LogoutController.java
@@ -0,0 +1,31 @@
+package com.zhengcheng.mall.admin.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zhengcheng.mall.admin.common.interceptor.LoginInterceptor;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 退出登录
+ *
+ * @author :    zhangquansheng
+ * @date :    2020/1/9 17:18
+ */
+@Api(tags = {"退出登录"})
+@Controller
+@RequestMapping("/logout")
+public class LogoutController {
+
+    @ApiOperation("退出页面")
+    @RequestMapping
+    public String logout(HttpSession session) {
+        session.removeAttribute(LoginInterceptor.PRINCIPAL_ATTRIBUTE_NAME);
+        return "redirect:/";
+    }
+
+}
