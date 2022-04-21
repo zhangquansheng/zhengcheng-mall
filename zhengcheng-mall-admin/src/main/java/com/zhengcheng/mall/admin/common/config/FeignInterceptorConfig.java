11f7629ac03def8a98e8e15cbc552a6c775b2af7
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-04-21 17:11:13 +0800

    satoken 透传

---
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/common/config/FeignInterceptorConfig.java
@@ -0,0 +1,39 @@
+package com.zhengcheng.mall.admin.common.config;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import cn.hutool.core.util.StrUtil;
+import feign.RequestInterceptor;
+
+/**
+ * FeignInterceptorConfig
+ *
+ * @author quansheng1.zhang
+ * @since 2022/4/21 16:56
+ */
+@Component
+public class FeignInterceptorConfig {
+
+    /**
+     * 使用feign client访问别的微服务时，将satoken放入参数或者header
+     */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        RequestInterceptor requestInterceptor = template -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                String satoken = request.getHeader("satoken");
+                if(StrUtil.isNotBlank(satoken)){
+                    template.header("satoken", satoken);
+                }
+            }
+        };
+        return requestInterceptor;
+    }
+}
