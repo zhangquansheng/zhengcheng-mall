10cfbc909c52b552f9328f2a91aa56225e36fd1f
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-04-22 00:04:26 +0800

    删除角色

---
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/common/advice/AdminExceptionControllerAdvice.java
@@ -0,0 +1,34 @@
+package com.zhengcheng.mall.admin.common.advice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.zhengcheng.common.web.CodeEnum;
+import com.zhengcheng.common.web.Result;
+import com.zhengcheng.core.advice.ExceptionControllerAdvice;
+
+import cn.dev33.satoken.exception.NotPermissionException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * AdminExceptionControllerAdvice
+ *
+ * @author quansheng1.zhang
+ * @since 2022/4/21 23:59
+ */
+@Slf4j
+@Configuration
+@RestControllerAdvice
+public class AdminExceptionControllerAdvice extends ExceptionControllerAdvice {
+
+    @ExceptionHandler({NotPermissionException.class})
+    @ResponseStatus(HttpStatus.OK)
+    public Result<Void> handleMaxUploadSizeExceededException(NotPermissionException e) {
+        log.warn("NotPermissionException code :{} , message:{} ",e.getCode(), e.getMessage());
+        return Result.create(CodeEnum.INTERNAL_SERVER_ERROR.getCode(), e.getMessage());
+    }
+
+}
