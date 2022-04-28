b9192e18100510f27e562154b42b24f7354d3240
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-04-28 20:53:04 +0800

    数据字典

---
+++ zhengcheng-mall-common/src/main/java/com/zhengcheng/mall/domain/mapper/DictDataMapper.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mall.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhengcheng.mall.domain.entity.DictData;
+
+/**
+ * DictDataMapper
+ *
+ * @author quansheng1.zhang
+ * @since 2022/4/28 20:52
+ */
+public interface DictDataMapper extends BaseMapper<DictData> {
+}
