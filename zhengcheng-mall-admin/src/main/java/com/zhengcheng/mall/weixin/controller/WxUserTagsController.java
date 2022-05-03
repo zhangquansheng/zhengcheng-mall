b3d85f38186457ae732ed3e166fb0853403dee56
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-03 22:06:23 +0800

    后台，微信公众号管理

--- zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/weixin/controller/WxUserTagsController.java
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/weixin/controller/WxUserTagsController.java
@@ -1,11 +1,21 @@
 package com.zhengcheng.mall.weixin.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zhengcheng.common.web.Result;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpUserTagService;
+import me.chanjar.weixin.mp.bean.tag.WxUserTag;
 
 /**
  * 微信用户标签
@@ -18,10 +28,24 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/admin/wxusertags")
 public class WxUserTagsController {
 
+    @Autowired
+    private WxMpService wxService;
+
     @ApiOperation("微信用户标签页面")
     @GetMapping("/view")
     public String view() {
         return "/view/system/wxusertags";
     }
 
+    @GetMapping("/list")
+    public @ResponseBody Result<List<WxUserTag>> getWxusertags(String appId) {
+        WxMpUserTagService wxMpUserTagService = wxService.getUserTagService();
+        try {
+            List<WxUserTag> listWxUserTag = wxMpUserTagService.tagGet();
+            return Result.successData(listWxUserTag);
+        } catch (WxErrorException e) {
+            log.error("获取微信用户标签失败", e);
+            return Result.errorMessage(e.getMessage());
+        }
+    }
 }
