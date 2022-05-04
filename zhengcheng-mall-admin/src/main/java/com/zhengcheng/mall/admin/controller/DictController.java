249ffe962b726814e72266d672c458bf82036d60
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-04 23:41:23 +0800

    数据字典

--- zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/controller/DictController.java
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/controller/DictController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import com.zhengcheng.common.web.PageCommand;
@@ -36,6 +37,31 @@ public class DictController {
     @GetMapping("/view")
     public String view() {
         return "/view/system/dict";
+    }
+
+    @ApiOperation("添加字典数据")
+    @GetMapping("/addData")
+    public String addData(String typeCode, Model model) {
+        model.addAttribute("typeCode", typeCode);
+        return "/view/system/dict/addData";
+    }
+
+    @ApiOperation("编辑字典数据")
+    @GetMapping("/editData")
+    public String editData() {
+        return "/view/system/dict/editData";
+    }
+
+    @ApiOperation("添加字典类型")
+    @GetMapping("/addType")
+    public String addType() {
+        return "/view/system/dict/addType";
+    }
+
+    @ApiOperation("编辑字典类型")
+    @GetMapping("/editType")
+    public String editType() {
+        return "/view/system/dict/editType";
     }
 
     @ApiOperation("分页查询字典类型")
