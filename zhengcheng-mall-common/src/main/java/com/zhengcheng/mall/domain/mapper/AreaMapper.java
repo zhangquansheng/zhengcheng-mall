3e3a33e6597dcc70ff64fb7c2e0cccebd7689955
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-10 11:21:14 +0800

    地区基础数据维护

---
+++ zhengcheng-mall-common/src/main/java/com/zhengcheng/mall/domain/mapper/AreaMapper.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mall.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhengcheng.mall.domain.entity.Area;
+
+/**
+ * AreaMapper
+ *
+ * @author quansheng1.zhang
+ * @since 2022/5/10 10:58
+ */
+public interface AreaMapper extends BaseMapper<Area> {
+}
---
+++ zhengcheng-mall-common/src/main/java/com/zhengcheng/mall/service/AreaService.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mall.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhengcheng.mall.domain.entity.Area;
+
+/**
+ * AreaService
+ *
+ * @author quansheng1.zhang
+ * @since 2022/5/10 11:00
+ */
+public interface AreaService extends IService<Area> {
+}
