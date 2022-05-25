fd54312f25686b1e94fd9ba0e386bb420cdf1450
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-25 13:35:16 +0800

    第三方登录

---
+++ zhengcheng-mall-admin/src/main/java/com/zhengcheng/mall/admin/controller/TestController.java
@@ -0,0 +1,58 @@
+package com.zhengcheng.mall.admin.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.xkcoding.justauth.AuthRequestFactory;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.request.AuthRequest;
+import me.zhyd.oauth.utils.AuthStateUtils;
+
+/**
+ * <p>
+ * 测试 Controller
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019-07-22 11:17
+ */
+@Slf4j
+@RestController
+@RequestMapping("/oauth")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class TestController {
+    private final AuthRequestFactory factory;
+
+    @GetMapping
+    public List<String> list() {
+        return factory.oauthList();
+    }
+
+    @GetMapping("/login/{type}")
+    public void login(@PathVariable String type, HttpServletResponse response) throws IOException {
+        AuthRequest authRequest = factory.get(type);
+        response.sendRedirect(authRequest.authorize(AuthStateUtils.createState()));
+    }
+
+    @RequestMapping("/{type}/callback")
+    public AuthResponse login(@PathVariable String type, AuthCallback callback) {
+        AuthRequest authRequest = factory.get(type);
+        AuthResponse response = authRequest.login(callback);
+        log.info("【response】= {}", JSON.toJSONString(response));
+        return response;
+    }
+
+}
