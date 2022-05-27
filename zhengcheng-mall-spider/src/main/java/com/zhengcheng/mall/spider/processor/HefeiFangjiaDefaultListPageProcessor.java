9adc40a12f4fb539babc614cdaf4c81ed1c5cb74
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-27 22:24:39 +0800

    爬虫接口

--- zhengcheng-mall-spider/src/main/java/com/zhengcheng/mall/spider/processor/HefeiFangjiaDefaultListPageProcessor.java
+++ zhengcheng-mall-spider/src/main/java/com/zhengcheng/mall/spider/processor/HefeiFangjiaDefaultListPageProcessor.java
@@ -2,6 +2,7 @@ package com.zhengcheng.mall.spider.processor;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 import com.zhengcheng.common.web.PageCommand;
 import com.zhengcheng.mall.spider.dto.House;
@@ -27,11 +28,16 @@ import us.codecraft.webmagic.utils.HttpConstant;
 public class HefeiFangjiaDefaultListPageProcessor implements PageProcessor {
 
     // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
-    private Site        site = Site.me().setRetryTimes(3).setSleepTime(500);
+    private Site        site           = Site.me().setRetryTimes(3).setSleepTime(500);
 
     private PageCommand pageCommand;
 
     private List<House> houses;
+
+    /**
+     * 当前页
+     */
+    private Integer     currentPageNum = 1;
 
     public HefeiFangjiaDefaultListPageProcessor(PageCommand pageCommand, List<House> houses) {
         this.pageCommand = pageCommand;
@@ -40,29 +46,38 @@ public class HefeiFangjiaDefaultListPageProcessor implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        for (int x = 0; x < 15; x++) {
-            String recordNum = page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/text()", x, 1)).get();
-            if (StrUtil.isNotBlank(recordNum)) {
-                House house = new House();
-                Integer lpId = Integer.parseInt(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/@href", x, 1))
-                        .get().replace("Detail2.aspx?Id=", "").trim());
-                house.setLpId(lpId);
-                house.setRecordNum(NumberUtil.parseInt(recordNum.trim()));
-                house.setName(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/text()", x, 2)).get().trim());
-                house.setBuildingNum(
-                        page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 3)).get().trim());
-                house.setBuiltUpArea(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 4)).get());
-                house.setRoomNum(Integer
-                        .valueOf(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 5)).get().trim()));
-                house.setAveragePrice(NumberUtil.parseInt(page.getHtml()
-                        .xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 6)).get().replace(",", "").trim()));
-                log.info("成功抓取：{}", JSONUtil.toJsonStr(house));
+        log.info("当前分页数:{}", currentPageNum);
+        // 指定页面，抓取数据
+        if (Objects.equals(pageCommand.getPageNo(), currentPageNum)) {
+            for (int x = 0; x < 15; x++) {
+                String recordNum = page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/text()", x, 1)).get();
+                if (StrUtil.isNotBlank(recordNum)) {
+                    House house = new House();
+                    Integer lpId = Integer
+                            .parseInt(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/@href", x, 1)).get()
+                                    .replace("Detail2.aspx?Id=", "").trim());
+                    house.setLpId(lpId);
+                    house.setRecordNum(NumberUtil.parseInt(recordNum.trim()));
+                    house.setName(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/a/text()", x, 2)).get().trim());
+                    house.setBuildingNum(
+                            page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 3)).get().trim());
+                    house.setBuiltUpArea(page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 4)).get());
+                    house.setRoomNum(Integer.valueOf(
+                            page.getHtml().xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 5)).get().trim()));
+                    house.setAveragePrice(NumberUtil.parseInt(page.getHtml()
+                            .xpath(StrUtil.format("//tr[{}]//td[{}]/text()", x, 6)).get().replace(",", "").trim()));
+                    log.info("成功抓取：{}", JSONUtil.toJsonStr(house));
 
-                houses.add(house);
+                    houses.add(house);
+                }
             }
         }
 
-        // 部分三：从页面发现后续的url地址来抓取
+        if (pageCommand.getPageNo() <= 1 || currentPageNum >= pageCommand.getPageNo()) {
+            log.info("是第一页或者当前抓取的页面已等于指定的页面，则退出！currentPageNum:{},pageNo:{}", currentPageNum, pageCommand.getPageNo());
+            return;
+        }
+        // 部分三：从页面发现后续的url地址来抓取（非第一页）
         String viewState = page.getHtml().xpath("//input[@id=__VIEWSTATE]/@value").get();
         String eventValidation = page.getHtml().xpath("//input[@id=__EVENTVALIDATION]/@value").get();
 
@@ -72,6 +87,7 @@ public class HefeiFangjiaDefaultListPageProcessor implements PageProcessor {
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36");
         request.setCharset("utf-8");
         HashMap<String, Object> params = new HashMap<>();
+        // 跳转到第几页
         params.put("__EVENTARGUMENT", pageCommand.getPageNo());
         params.put("__EVENTTARGET", "AspNetPager1");
         params.put("__VIEWSTATE", viewState);
@@ -80,6 +96,9 @@ public class HefeiFangjiaDefaultListPageProcessor implements PageProcessor {
         request.setRequestBody(HttpRequestBody.form(params, "utf-8"));
 
         page.addTargetRequest(request);
+
+        // 设置当前页
+        currentPageNum = pageCommand.getPageNo();
     }
 
     @Override
