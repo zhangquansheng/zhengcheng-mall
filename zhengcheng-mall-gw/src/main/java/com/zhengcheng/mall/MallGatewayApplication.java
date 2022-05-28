b11888f7cfc7a6df24636860e020ed0f0b6eefba
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-05-28 11:50:07 +0800

    网关应用

--- zhengcheng-mall-gw/src/main/java/com/zhengcheng/mall/MallGatewayApplication.java
+++ zhengcheng-mall-gw/src/main/java/com/zhengcheng/mall/MallGatewayApplication.java
@@ -2,6 +2,7 @@ package com.zhengcheng.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * GatewayApplication
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author quansheng1.zhang
  * @since 2022/3/24 22:23
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallGatewayApplication {
 
