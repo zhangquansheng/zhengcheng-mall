5d242c32132a0aab60a2539197b79b79f2f08808
quansheng1.zhang <quansheng1.zhang@zhangmen.com>
2022-06-02 22:21:49 +0800

    ES

--- zhengcheng-mall-es/src/main/java/com/zhengcheng/mall/MallEsApplication.java
+++ zhengcheng-mall-es/src/main/java/com/zhengcheng/mall/MallEsApplication.java
@@ -2,20 +2,18 @@ package com.zhengcheng.mall;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
- * MallBbsApplication
+ * MallEsApplication
  *
  * @author quansheng1.zhang
  * @since 2022/3/8 17:36
  */
-@EnableCaching
 @SpringBootApplication
-public class MallBbsApplication {
+public class MallEsApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(MallBbsApplication.class).run(args);
+        new SpringApplicationBuilder(MallEsApplication.class).run(args);
     }
 
 }
