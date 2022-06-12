45359a0a59dc7729243dd710a868b75d49d34e40
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-12 15:20:34 +0800

    抖音账号管理

---
+++ zhengcheng-mall-common/src/main/java/com/zhengcheng/mall/domain/mapper/DouyinUserGroupMapper.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mall.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhengcheng.mall.domain.entity.DouyinUserGroup;
+
+/**
+ * DouyinUserGroupMapper
+ *
+ * @author quansheng1.zhang
+ * @since 2022/6/12 15:17
+ */
+public interface DouyinUserGroupMapper extends BaseMapper<DouyinUserGroup> {
+}
---
+++ zhengcheng-mall-common/src/main/java/com/zhengcheng/mall/domain/mapper/DouyinUserMapper.java
@@ -0,0 +1,13 @@
+package com.zhengcheng.mall.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhengcheng.mall.domain.entity.DouyinUser;
+
+/**
+ * DouyinUserMapper
+ *
+ * @author quansheng1.zhang
+ * @since 2022/6/12 15:16
+ */
+public interface DouyinUserMapper extends BaseMapper<DouyinUser> {
+}
