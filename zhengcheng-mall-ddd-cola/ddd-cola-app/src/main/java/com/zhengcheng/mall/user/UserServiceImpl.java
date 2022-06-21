c7f4e869e953670d3c05efd38fb6f632bab59083
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-21 14:55:51 +0800

    DDD 1.0-SNAPSHOT

---
+++ zhengcheng-mall-ddd-cola/ddd-cola-app/src/main/java/com/zhengcheng/mall/user/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.zhengcheng.mall.user;
+
+import com.zhengcheng.mall.api.UserService;
+import com.zhengcheng.mall.dto.UserAddCmd;
+
+/**
+ * UserServiceImpl
+ *
+ * @author quansheng1.zhang
+ * @since 2022/6/21 10:33
+ */
+@Service
+@CatchAndLog
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public Long add(UserAddCmd userAddCmd) {
+        return null;
+    }
+
+}
