be623882d9dfbf5dbf5e86d60cc1c6f0c06f5ecc
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-27 14:20:35 +0800

    阿里巴巴的 cola 脚手架 4.0 版本

---
+++ zhengcheng-mall-ddd-cola/ddd-cola-infrastructure/src/main/java/com/zhengcheng/mall/order/gatewayimpl/OrderGatewayImpl.java
@@ -0,0 +1,15 @@
+package com.zhengcheng.mall.order.gatewayimpl;
+
+import org.springframework.stereotype.Component;
+
+import com.zhengcheng.mall.order.gateway.OrderGateway;
+
+/**
+ * OrderGatewayImpl
+ *
+ * @author quansheng1.zhang
+ * @since 2022/6/27 14:19
+ */
+@Component
+public class OrderGatewayImpl implements OrderGateway {
+}
