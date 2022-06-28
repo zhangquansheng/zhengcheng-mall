71f2d8ba78310b04219554f0f7a7c7de7b159ed2
yl-zhangquansheng <zhangquansheng@za-doctor.com>
2022-06-28 12:28:15 +0800

    HelloController

--- zhengcheng-mall-api/src/main/java/com/zhengcheng/mall/api/controller/HelloController.java
+++ zhengcheng-mall-api/src/main/java/com/zhengcheng/mall/api/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.zhengcheng.mall.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.zhengcheng.common.web.Result;
 
 /**
